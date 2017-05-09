08d76a1f6b0ee2f989decebf6ab045c6370cc3d5
luisvalesilva <luisvalesilva@gmail.com>
2017-05-09 10:48:51 -0400

    Added mean normalization and chromosome ordering arguments

--- R/average_signal.R
+++ R/average_signal.R
@@ -7,20 +7,27 @@
 #' \code{?"GRanges-class"} for more details). To load wiggle and bedGraph data
 #' run \code{\link{import_wiggle}} and \code{\link{import_bedGraph}},
 #' respectively. No default.
-#' @return List of two named numeric vectors with average \code{score} on:
+#' @param mean_norm Logical indicating whether to normalize by average
+#' genome-wide score. This adds two columns to the dataframe (first element of
+#' the output list): mean ratio (\code{avrg_signal_mean_ratio}) and
+#' mean-subtracted (\code{avrg_signal_mean_subtracted}).
+#' Defaults to \code{FALSE}.
+#' @param order_chrs Logical indicating whether to order rows by chromosome
+#' length (in increasing order). Defaults to \code{FALSE}.
+#' @return List with two elements:
 #' \enumerate{
-#'   \item \code{seq_avrg} Each individual sequence
-#'   \item \code{genome_avrg} Whole genome
+#'   \item \code{seq_avrg} Average \code{score} on each sequence (dataframe)
+#'   \item \code{genome_avrg} Average \code{score} genome-wide (named vector)
 #' }
 #' @examples
 #' \dontrun{
-#' average_signal(GRanges_objct)
+#' average_signal(GRanges_object)
 #' 
-#' average_signal(WT)
+#' average_signal(WT, mean_norm=TRUE, order_chrs=TRUE)
 #' }
 #' @export
 
-average_signal <- function(gr){
+average_signal <- function(gr, mean_norm = FALSE, order_chrs=FALSE){
   # IO checks
   check_package("GenomicRanges")
   if (!is(gr, "GRanges")) stop('input must be a GRanges object.')
@@ -32,9 +39,52 @@ average_signal <- function(gr){
   avrg <- function(x) (sum(GenomicRanges::width(x) * GenomicRanges::score(x))
                       / sum(GenomicRanges::width(x)))
   genome_avrg <- avrg(gr)
-  seq_avrg <- sapply(GenomicRanges::split(gr,
-                                          GenomicRanges::seqnames(gr)), avrg)
+  seq_avrg <- sapply(GenomicRanges::split(gr, GenomicRanges::seqnames(gr)),
+                     avrg)
+  
+  # Convert to dataframe
+  seq_avrg <- data.frame(chr=names(seq_avrg), avrg_signal=seq_avrg,
+                         row.names=NULL, stringsAsFactors = F)
+  
+  if (mean_norm) {
+    seq_avrg$avrg_signal_mean_subtracted <- seq_avrg$avrg_signal - genome_avrg
+    seq_avrg$avrg_signal_mean_ratio <- seq_avrg$avrg_signal / genome_avrg
+  }
+  
+  if (order_chrs) seq_avrg <- order_chromosomes(seq_avrg, chr_column='chr',
+                                                decreasing=FALSE)
   
   message('Done!')
   return(list("seq_avrg"=seq_avrg, "genome_avrg"=genome_avrg))
+}
+
+
+order_chromosomes <- function(df, chr_column, decreasing=FALSE) {
+  # Make sure input is a data frame with 16 rows
+  if (!is.data.frame(df) | nrow(df) != 16) {
+    stop("Wrong input data - not an R data frame wit 16 rows.", call. = FALSE)
+  }
+  
+  # Check reference genome and get order
+  chrom_S288C <- c('chrI', 'chrVI', 'chrIII', 'chrIX', 'chrVIII', 'chrV',
+                   'chrXI', 'chrX', 'chrXIV', 'chrII', 'chrXIII', 'chrXVI',
+                   'chrXII', 'chrVII', 'chrXV', 'chrIV')
+  chrom_SK1 <- c('chr01', 'chr06', 'chr03', 'chr09', 'chr08', 'chr05', 'chr11',
+                 'chr10', 'chr14', 'chr02', 'chr13', 'chr16', 'chr12', 'chr07',
+                 'chr15', 'chr04')
+  
+  check_S288C <- any(grep('chr[XVI]', df[, chr_column]))
+  check_SK1 <- any(grep('chr[0-9]', df[, chr_column]))
+  
+  if (check_S288C && !check_SK1) {
+    chrom <- chrom_S288C    
+  } else if (check_SK1 && !check_S288C) {
+    chrom <- chrom_SK1
+  } else stop('Did not recognize reference genome.\n',
+              'Please make sure it is in one of the following two formats:\n',
+              '"chrI" or "chr01"', call. = FALSE)
+  
+  if (decreasing) chrom <- rev(chrom)
+  
+  return(df[match(chrom, df[[chr_column]]), ])
 }
