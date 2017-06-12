4f1925bdc7519366dba83a36bdd010d2e36d027c
luisvalesilva <luisvalesilva@gmail.com>
2017-06-12 13:28:09 -0400

    Refactored to output EnrichedHeatmap matrix

--- R/signal_at_orf2.R
+++ R/signal_at_orf2.R
@@ -18,14 +18,14 @@
 #' @param gff Either a path to a gff file or loaded gff data as a \code{GRanges}
 #' object. No default.
 #' @param write_to_file Logical indicating whether output should be written to a
-#' .txt file (in current working directory). If \code{to_file = FALSE} the
-#' function returns output R object. Defaults to \code{FALSE}.
-#' @return A data frame with 1001 columns:
-#' \enumerate{
-#'   \item \code{gene} First column with the gene IDs (if found in input gff)
-#'   \item \code{t1 to t1000} 1000 columns with signal at each meta position.
-#' }
-#' Columns t1 to t1000 correspond to the following meta positions:
+#' .txt file. The file will be saved at location provided in "file_name"
+#' argument or at the current working directory if only a file name is provided).
+#' If \code{write_to_file = FALSE} the function returns output R object.
+#' Defaults to \code{FALSE}.
+#' @param file_name Charatcer string indicating file name when writing output to
+#' a .txt file. Must be provided if \code{write_to_file = TRUE}. No default.
+#' @return An \code{EnrichedHeatmap} matrix with 1000 columns containing signal
+#' at each meta position. Columns correspond to the following meta positions:
 #' \enumerate{
 #'   \item \code{t1 to t250} Upstream flank (1/2 of gene length)
 #'   \item \code{t251 to t750} ORF
@@ -36,11 +36,11 @@
 #' signal_at_orf2(WT, gff = gff)
 #' 
 #' signal_at_orf2(WT, gff = "S288C_annotation_modified.gff",
-#'                    write_to_file = TRUE)
+#'                write_to_file = TRUE, file_name = "WT_S288C_metaORF.txt")
 #' }
 #' @export
 
-signal_at_orf2 <- function(signal_data, gff, write_to_file=FALSE) {
+signal_at_orf2 <- function(signal_data, gff, write_to_file=FALSE, file_name) {
   t0  <- proc.time()
   
   # IO checks
@@ -55,17 +55,27 @@ signal_at_orf2 <- function(signal_data, gff, write_to_file=FALSE) {
                          '"gff" must be gff data as a "GRanges" object\n',
                          'or the path to a gff file.', call. = FALSE)
   
-  if (!is(gff, "GRanges") && is(gff, "character")) {
-    check_path(gff)
-    check_package("rtracklayer")
-    message('Loading gff file...')
-    gff <- rtracklayer::import.gff(gff)
-  } else stop('"gff" must be either a GRanges object or a path to a gff file.')
+  if (!is(gff, "GRanges")) {
+    if (is(gff, "character")) {
+      check_path(gff)
+      check_package("rtracklayer")
+      message('Loading gff file...')
+      gff <- rtracklayer::import.gff(gff)
+    } else {
+      stop('"gff" must be either a GRanges object or a path to a gff file.')
+    }
+  }
+  
+  if (write_to_file & missing(file_name)) {
+    stop('No file name provided.\n',
+         '"file_name" must be be provided when "write_to_file = TRUE"',
+         call. = FALSE)
+  }
   
   # Drop 'chrMito' and '2-micron' if present in gff (absent from ChIP-seq data)
   gff <- GenomeInfoDb::dropSeqlevels(gff, c('chrMito', '2-micron'))
   
-  # Check reference genome (must match between input data and gff)
+  # Check seqnames (must match between input data and gff)
   if (check_genome(signal_data)[1] != check_genome(gff)[1]) {
     stop("The reference genomes in the data and the gff do not seem to match.",
          call. = FALSE)
@@ -97,37 +107,32 @@ signal_at_orf2 <- function(signal_data, gff, write_to_file=FALSE) {
                                             extend=0, k=1000, empty_value=NA,
                                             smooth=FALSE, target_ratio=1)
   
-  ### Add gene names to data
-  # GFF files are not guaranteed to contain a consistent column name for gene ID
-  # Check columns in provided GFF in search for the gene ID column
-  if ('group' %in% names(GenomicRanges::mcols(gff))) {
-    df <- cbind("gene"=gff$group, as.data.frame(mat))
-  } else if ('ID' %in% names(GenomicRanges::mcols(gff))) {
-    df <- cbind("gene"=gff$ID, as.data.frame(mat))
-  } else {
-    message('Could not find gene IDs in the provided gff')
-    message('(the output will not include gene IDs).')
-    df <- as.data.frame(mat)
-  }
-  
   # Check NAs
-  n_genes <- nrow(df)
-  n_complete_genes <- nrow(df[complete.cases(df), ])
+  n_genes <- nrow(mat)
+  n_complete_genes <- nrow(mat[complete.cases(mat), ])
   n_genes_with_NAs <- n_genes - n_complete_genes
   message('---')
   message('Genes containing NA values: ', n_genes_with_NAs,
           ' of a total of ', n_genes, ' (',
           round(n_genes_with_NAs / n_genes * 100, 1), '%)')
-  
-  message('---')
-  message('Completed in ', hwglabr2::elapsed_time(t0))
-  
+
   if (write_to_file){
-    file_name <- paste0(deparse(substitute(signal_data)), "_",
-                        check_genome(signal_data)[1], "_metaORF.txt")
     message(paste0('Writing data to file: ', file_name))
-    write.table(df, file_name, sep="\t", quote=FALSE, row.names=FALSE)
+    write.table(mat, file_name, sep="\t", quote=FALSE, row.names=FALSE)
     
-    message('Done!')
-  } else return(df)
+    message('---')
+    message('Completed in ', hwglabr2::elapsed_time(t0))
+  } else {
+    # Adjust attributes of EnrichedHeatmap matrix:
+    attr(mat, "upstream_index") <- 1:250
+    attr(mat, "target_index") <- 251:750
+    attr(mat, "downstream_index") <- 751:1000
+    attr(mat, "extend") <- c('', '') # Leave x axis 1 and 1000 annotations blank
+    attr(mat, "signal_name") <- deparse(substitute(signal_data))
+    attr(mat, "target_name") <- 'ORFs'
+    
+    message('---')
+    message('Completed in ', hwglabr2::elapsed_time(t0))
+    return(mat)
+    }
 }
