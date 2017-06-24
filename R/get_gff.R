9560fd151dca71d1107f2e2335fcc8e590d24773
luisvalesilva <luisvalesilva@gmail.com>
2017-06-24 16:02:33 -0400

    First commit

---
+++ R/get_gff.R
@@ -0,0 +1,35 @@
+#' Get GFF
+#'
+#' Returns GFF table for specified genome.
+#' @param genome Character object specifying the genome version; accepts one of
+#' the following options:
+#' \enumerate{
+#'   \item \code{"SK1Yue"}
+#'   \item \code{"sacCer3"}
+#'   \item \code{"SK1"}
+#' }
+#' No default.
+#' @return GFF table as \code{GRanges} object.
+#' @examples
+#' \dontrun{
+#' get_gff(genome='SK1Yue')
+#' 
+#' get_gff(genome='sacCer3')
+#' }
+#' @export
+
+get_gff <- function(genome){
+  # IO checks
+  check_package("GenomicRanges")
+  if (!is(genome, "character")) stop('"genome" must be a character object.')
+  
+  if (genome == 'SK1Yue') {
+      gff <- SK1Yue_gff
+  } else if (genome == 'sacCer3') {
+      gff <- sacCer3_gff
+  } else if (genome == 'SK1') {
+      gff <- SK1_gff
+  } else stop('"genome" argument must be one of "SK1Yue", "sacCer3" or "SK1".')
+    
+  return(gff)
+}
