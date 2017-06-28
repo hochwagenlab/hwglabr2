f8f1e46aa94ec0ba0b69f3b84ee218b480fbea3d
luisvalesilva <luisvalesilva@gmail.com>
2017-06-28 19:01:23 -0400

    Allow conversion of SK1Yue data to GRanges class

--- R/get_intergenic_regions.R
+++ R/get_intergenic_regions.R
@@ -9,6 +9,11 @@
 #'   \item \code{"SK1"}
 #' }
 #' No default.
+#' @param as_gr Logical specifying whether the output should be returned as a
+#' \code{GRanges} object. If \code{FALSE}, output is a \code{data.frame}. Can
+#' only be used if \code{genome="SK1Yue"}, because \code{"SK1"} and
+#' \code{"sacCer3"} contain overlapping genes, resulting in negative intervals
+#' (not allowed by \code{IRanges}). Defaults to \code{FALSE}.
 #' @return R data frame containing coordinates and annotations for all
 #' intergenic regions in the genome.
 #' @examples
@@ -20,8 +25,11 @@
 #' }
 #' @export
 
-get_intergenic_regions <- function(genome){
+get_intergenic_regions <- function(genome, as_gr=FALSE){
   # IO checks
+  if (as_gr & genome != 'SK1Yue') {
+    stop('"as_gr" can only be used when genome="SK1Yue".', call. = FALSE)
+  }
   if (!is(genome, "character")) stop('"genome" must be a character object.')
   
   if (genome == 'SK1Yue') {
@@ -31,6 +39,19 @@ get_intergenic_regions <- function(genome){
   } else if (genome == 'SK1') {
     inter_table <- SK1_intergenic
   } else stop('"genome" argument must be one of "SK1Yue", "sacCer3" or "SK1".')
+  
+  if (as_gr) {
+    inter_table <- GenomicRanges::GRanges(
+      seqnames=inter_table$chr,
+      ranges=IRanges::IRanges(start=inter_table$left_coordinate,
+                              end=inter_table$right_coordinate),
+      strand="+", type=inter_table$type,
+      left_gene=inter_table$left_gene,
+      left_gene_strand=inter_table$left_gene_strand,
+      right_gene=inter_table$right_gene,
+      right_gene_strand=inter_table$right_gene_strand
+    )
+  }
 
   return(inter_table)
 }
