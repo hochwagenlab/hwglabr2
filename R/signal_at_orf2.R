eef7595a8e7aaad6c36860bb14e293fa939ff7a7
Luís <luisvalesilva@gmail.com>
2017-11-10 14:54:09 -0500

    More general code for matrix attributes

--- R/signal_at_orf2.R
+++ R/signal_at_orf2.R
@@ -152,12 +152,12 @@ signal_at_orf2 <- function(signal_data, gff, window_size = 1,
     message('Completed in ', hwglabr2::elapsed_time(t0, proc.time()[3]))
   } else {
     # Adjust attributes of EnrichedHeatmap matrix:
-    start <- floor(250 / window_size)
-    end <- floor(750 / window_size)
+    quarter1 <- round(n_windows / 4)
+    quarter3 <- quarter1 * 3
     
-    attr(mat, "upstream_index") <- 1:start
-    attr(mat, "target_index") <- (start + 1):end
-    attr(mat, "downstream_index") <- (end + 1):n_windows
+    attr(mat, "upstream_index") <- 1:quarter1
+    attr(mat, "target_index") <- (quarter1 + 1):quarter3
+    attr(mat, "downstream_index") <- (quarter3 + 1):n_windows
     attr(mat, "extend") <- c('', '') # Leave x axis 1 and 1000 annotations blank
     attr(mat, "signal_name") <- deparse(substitute(signal_data))
     attr(mat, "target_name") <- 'ORFs'
