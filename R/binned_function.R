cda4a6fb2d4824d8dbd36c9b00d9ac80650c7e80
luisvalesilva <luisvalesilva@gmail.com>
2018-01-09 18:19:11 -0500

    Removed unused varname argument

--- R/binned_function.R
+++ R/binned_function.R
@@ -2,21 +2,26 @@
 #'
 #' This is a generalization of \code{GenomicRanges::binnedAverage} to allow the
 #' use of any applicable function instead of simply the mean. The interface is
-#' the same with the added argument for the function to use.
+#' very similar, with two differences:
+#' \enumerate{
+#'   \item Added argument \code{fun}, for the function to use.
+#'   \item Dropped argument \code{varname}, since this function outputs the
+#'   metadata as a vector, rather than the complete \code{GRanges} object with
+#'   the metadata column added and named as indicated by \code{varname}. 
+#'   \item \code{"SK1"}
+#' }
 #' @param bins \code{GRanges} object representing the genomic bins. Typically
 #' obtained by calling \code{tileGenome} with
 #' \code{cut.last.tile.in.chrom=TRUE}. No default.
 #' @param numvar Named RleList object representing a numerical variable defined
 #' along the genome covered by bins (which is the genome described by
 #' \code{seqinfo(bins)}). No default.
-#' @param varname Character string indicating the name of the genomic variable.
-#' No default.
 #' @param fun R function to apply to the numerical variable in each bin.
 #' No default.
 #' @param ... optional arguments to \code{fun}.
 #' @examples
 #' \dontrun{
-#' # Generate 100-bp genome tiles
+#' # Generate 100-bp genome tiles for a given GRanges object
 #' bins <- GenomicRanges::tileGenome(GenomeInfoDb::seqlengths(gr),
 #'                                   tilewidth=100, cut.last.tile.in.chrom=TRUE)
 #' 
@@ -24,12 +29,15 @@
 #' score <- GenomicRanges::coverage(gr, weight="score")
 #' 
 #' # Now use the function to get the maximum score in each tile
-#' max_per_bin <- binned_function(bins=bins, numvar=score,
-#'                                varname="binned_score", fun=max)
+#' gr$max_per_bin <- binned_function(bins=bins, numvar=score, fun=max)
+#' 
+#' # You can pass arguments to fun too
+#' gr$max_per_bin <- binned_function(bins=bins, numvar=score,
+#'                                   fun=max, na.rm=TRUE)
 #' }
 #' @export
 
-binned_function <- function(bins, numvar, varname, fun, ...) {
+binned_function <- function(bins, numvar, fun, ...) {
   t0  <- proc.time()[3]
   
   # IO checks
