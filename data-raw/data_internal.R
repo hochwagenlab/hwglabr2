334d7f74a8bb934e5f8be9bc3038fd5265caa077
Luis Vale Silva <luisvalesilva@gmail.com>
2018-03-28 16:40:06 -0400

    Reorganized some gff data

--- data-raw/data_internal.R
+++ data-raw/data_internal.R
@@ -19,18 +19,33 @@ add_genome_name_to_GR <- function(gr, name='SK1Yue') {
 
 #------------------------------------------------------------------------------#
 #                                GFF files                                     #
-path <- '/Volumes/LabShare/GenomeSequences/'
+path <- '/Volumes/LabShare/GenomeSequences'
 # 1. Import SK1Yue data
 SK1Yue_gff <- 'SK1_Yue_et_al_2017/Yue.SK1.genome.nuclear.mito.2micr.gff'
-SK1Yue_gff <- rtracklayer::import.gff3(paste0(path, SK1Yue_gff))
+SK1Yue_gff <- rtracklayer::import.gff3(file.path(path, SK1Yue_gff))
 SK1Yue_gff <- add_genome_name_to_GR(SK1Yue_gff, name='SK1Yue')
-# 2. Import S288C data
+# 2. Import S288CYue data
+# $ cd Google_Drive_NYU/LabShare_Luis/LabWork/GenomeSequences/
+# $ mkdir S288C_Yue_et_al_2017/
+# $ cd S288C_Yue_et_al_2017/
+# $ wget http://yjx1217.github.io/Yeast_PacBio_2016/data/Nuclear_GFF/S288c.all_feature.gff.gz
+# $ gunzip *
+S288CYue_gff <- rtracklayer::import.gff(
+  file.path('/Volumes/LabShare-2/Luis/LabWork/GenomeSequences',
+            'S288C_Yue_et_al_2017/S288c.all_feature.gff'))
+# 3. Import S288C data
 sacCer3_gff <- 's288C_annotation_R64_modified.gff'
-sacCer3_gff <- rtracklayer::import.gff(paste0(path, sacCer3_gff))
+sacCer3_gff <- rtracklayer::import.gff(file.path(path, sacCer3_gff))
 sacCer3_gff <- add_genome_name_to_GR(sacCer3_gff, name='sacCer3')
-# 3. Import SK1 data
+
+sacCer2_gff <- file.path(
+  'S288C_reference_genome_R61-1-1_20080605',
+  'saccharomyces_cerevisiae_R61-1-1_20080607_modified.gff')
+sacCer2_gff <- rtracklayer::import.gff(file.path(path, sacCer2_gff))
+sacCer2_gff <- add_genome_name_to_GR(sacCer2_gff, name='sacCer2')
+# 4. Import SK1 data
 SK1_gff <- 'SK1_MvO_V1___GENOME/SK1_annotation/SK1_annotation_modified_v2.gff'
-SK1_gff <- rtracklayer::import.gff(paste0(path, SK1_gff))
+SK1_gff <- rtracklayer::import.gff(file.path(path, SK1_gff))
 SK1_gff <- add_genome_name_to_GR(SK1_gff, name='SK1')
 
 
@@ -41,18 +56,15 @@ SK1_gff <- add_genome_name_to_GR(SK1_gff, name='SK1')
 # Chromosome lengths calculated using:
 #cat SK1.genome.fa | awk '$0 ~ ">" {print c; c=0;printf substr($0,2,100) "\t"; } \
 #$0 !~ ">" {c+=length($0);} END { print c; }'
-SK1Yuecen <- data.frame("Chromosome" = paste0('chr', as.roman(1:16)),
-                        "Start" = c(154628, 251815, 108708, 460752, 171136,
-                                    170910, 501251, 102251, 348027, 447447,
-                                    451859, 151679, 251000, 637019, 307189,
-                                    555578),
-                        "End" = c(154745, 251931, 108824, 460871, 171253,
-                                  171027, 501370, 102368, 348143, 447565, 451975,
-                                  151797, 251118, 637136, 307307, 555694),
-                        "LenChr" = c(228861, 829469, 340914, 1486921, 589812,
-                                     299318, 1080440, 542723, 449612, 753937,
-                                     690901, 1054145, 923535, 791982, 1053869,
-                                     946846))
+SK1Yuecen <- data.frame(
+  "Chromosome" = paste0('chr', as.roman(1:16)),
+  "Start" = c(154628, 251815, 108708, 460752, 171136, 170910, 501251, 102251,
+              348027, 447447, 451859, 151679, 251000, 637019, 307189, 555578),
+  "End" = c(154745, 251931, 108824, 460871, 171253, 171027, 501370, 102368,
+            348143, 447565, 451975, 151797, 251118, 637136, 307307, 555694),
+  "LenChr" = c(228861, 829469, 340914, 1486921, 589812, 299318, 1080440, 542723,
+               449612, 753937, 690901, 1054145, 923535, 791982, 1053869,
+               946846))
 
 SK1Yuecen <- with(SK1Yuecen,
                   GenomicRanges::GRanges(Chromosome,
@@ -66,27 +78,15 @@ SK1Yuecen <- add_genome_name_to_GR(SK1Yuecen, name='SK1Yue')
 # Chromosome lengths calculated using:
 #cat S288c_SK1_Yue.fa | awk '$0 ~ ">" {print c; c=0;printf substr($0,2,100) "\t"; } \
 #$0 !~ ">" {c+=length($0);} END { print c; }'
-
-## got centromere details:
-# $ cd Google_Drive_NYU/LabShare_Luis/LabWork/GenomeSequences/
-# $ mkdir S288C_Yue_et_al_2017/
-# $ cd S288C_Yue_et_al_2017/
-# $ wget http://yjx1217.github.io/Yeast_PacBio_2016/data/Nuclear_GFF/S288c.all_feature.gff.gz
-# $ gunzip *
-
-S288CYue_gff <- rtracklayer::import.gff(paste0('~/Google_Drive_NYU/LabShare_Luis/',
-                                               'LabWork/GenomeSequences/',
-                                               'S288C_Yue_et_al_2017/',
-                                               'S288c.all_feature.gff'))
 start <- S288CYue_gff[S288CYue_gff$type == 'centromere']@ranges@start
 end <- start + S288CYue_gff[S288CYue_gff$type == 'centromere']@ranges@width - 1
 
-S288CYuecen <- data.frame("Chromosome" = paste0('chr', as.roman(1:16)),
-                          "Start" = start, "End" = end,
-                          "LenChr" = c(219929, 813597, 341580, 1566853, 583092,
-                                       271539, 1091538, 581049, 440036, 751611,
-                                       666862, 1075542, 930506, 777615, 1091343,
-                                       954457))
+S288CYuecen <- data.frame(
+  "Chromosome" = paste0('chr', as.roman(1:16)),
+  "Start" = start, "End" = end,
+  "LenChr" = c(219929, 813597, 341580, 1566853, 583092, 271539, 1091538, 581049,
+               440036, 751611, 666862, 1075542, 930506, 777615, 1091343,
+               954457))
 
 S288CYuecen <- with(S288CYuecen,
                     GenomicRanges::GRanges(Chromosome,
@@ -97,23 +97,17 @@ S288CYuecen <- with(S288CYuecen,
 S288CYuecen <- add_genome_name_to_GR(S288CYuecen, name='S288cYue')
 
 # SK1 info based on Keeney lab genome sequence and annotation
-SK1cen <- data.frame("Chromosome" = c("chr01","chr02","chr03","chr04","chr05",
-                                      "chr06","chr07","chr08","chr09","chr10",
-                                      "chr11","chr12","chr13","chr14","chr15",
-                                      "chr16"),
-                     "Start" = c(137832, 226711, 128699, 463204, 157003, 162815,
-                                 505440, 95031, 346215, 415648, 452723, 137738,
-                                 249103, 616840, 307236, 553355),
-                     "End" = c(137948, 226826, 128779, 463321, 157119, 162931,
-                               505558, 95147, 346330, 415764, 452838, 137855,
-                               249221, 616956, 307353, 553467),
-                     "Mid" = c(137890, 226768, 128739, 463262, 157061, 162873,
-                               505499, 95089, 346272, 415706, 452780, 137796,
-                               249162, 616898, 307294, 553411),
-                     "LenChr" = c(203893, 794508, 342718, 1490682, 602514,
-                                  284456, 1067526, 544538, 435585, 719294,
-                                  687260, 1008248, 908607, 812465, 1054033,
-                                  921188))
+SK1cen <- data.frame(
+  "Chromosome" = paste0('chr', stringr::str_pad(1:16, 2, pad = "0")),
+  "Start" = c(137832, 226711, 128699, 463204, 157003, 162815, 505440, 95031,
+              346215, 415648, 452723, 137738, 249103, 616840, 307236, 553355),
+  "End" = c(137948, 226826, 128779, 463321, 157119, 162931, 505558, 95147,
+            346330, 415764, 452838, 137855, 249221, 616956, 307353, 553467),
+  "Mid" = c(137890, 226768, 128739, 463262, 157061, 162873, 505499, 95089,
+            346272, 415706, 452780, 137796, 249162, 616898, 307294, 553411),
+  "LenChr" = c(203893, 794508, 342718, 1490682, 602514, 284456, 1067526, 544538,
+               435585, 719294, 687260, 1008248, 908607, 812465, 1054033,
+               921188))
 
 SK1cen <- with(SK1cen, GenomicRanges::GRanges(Chromosome,
                                               IRanges::IRanges(Start + 1, End),
@@ -122,9 +116,10 @@ SK1cen <- with(SK1cen, GenomicRanges::GRanges(Chromosome,
 SK1cen <- add_genome_name_to_GR(SK1cen, name='SK1')
 
 # S288c
-path <- '/Volumes/LabShare/GenomeSequences/S288C_reference_genome_R64-1-1_20110203/'
-file <- 'saccharomyces_cerevisiae_R64-1-1_20110208.gff'
-sacCer3gff <- rtracklayer::import.gff(paste0(path, file))
+path <- file.path('/Volumes/LabShare/GenomeSequences',
+                  'S288C_reference_genome_R64-1-1_20110203',
+                  'saccharomyces_cerevisiae_R64-1-1_20110208.gff')
+sacCer3gff <- rtracklayer::import.gff(path)
 sacCer3cen <- sacCer3gff[sacCer3gff$type == 'centromere', ]
 sacCer3cen <- GenomeInfoDb::dropSeqlevels(sacCer3cen, c('chrMito', '2-micron'))
 GenomicRanges::mcols(sacCer3cen) <- NULL
@@ -139,10 +134,6 @@ sacCer3cen <- add_genome_name_to_GR(sacCer3cen, name='sacCer3')
 # Useful for analysis of hybrid strains and SK1 strain ChIP-seq spiked in with
 # SK288c cells
 
-S288CYue_gff <- rtracklayer::import.gff(paste0('~/Google_Drive_NYU/LabShare_Luis/',
-                                               'LabWork/GenomeSequences/',
-                                               'S288C_Yue_et_al_2017/',
-                                               'S288c.all_feature.gff'))
 start <- S288CYue_gff[S288CYue_gff$type == 'centromere']@ranges@start
 end <- start + S288CYue_gff[S288CYue_gff$type == 'centromere']@ranges@width - 1
 
